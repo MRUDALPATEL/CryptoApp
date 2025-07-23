42845d8d63784c0f962d5ff266778c299ba71ce2
MRUDALPATEL <mrudalpatel5336@gmail.com>
2025-07-23 15:49:12 +0530

    Routing and components creation done

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar } from "./components";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinTd" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
